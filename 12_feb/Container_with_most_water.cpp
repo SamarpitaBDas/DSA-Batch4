f83c9affad8ee1c47d0712b50d8be8449380db3c
SamarpitaBDas <samarpitabdas@gmail.com>
2026-03-12 23:33:05 +0530

    11. Container With Most Water -- two pointer

--- 12_feb/Container_with_most_water.cpp
+++ 12_feb/Container_with_most_water.cpp
@@ -4,10 +4,18 @@ using namespace std;
 int maxArea(vector<int>& height) {
     int maxArea = 0;
     //brute force
-    for(int i =0;i<height.size();i++){
-        for(int j = i+1;j<height.size();j++){
-            maxArea = max(maxArea, min(height[i],height[j])*(j-i));
-        }
+    // for(int i =0;i<height.size();i++){
+    //     for(int j = i+1;j<height.size();j++){
+    //         maxArea = max(maxArea, min(height[i],height[j])*(j-i));
+    //     }
+    // }
+
+    //two pointer approach
+    int i=0,j=height.size()-1;
+    while(i<j){
+        maxArea = max(maxArea, min(height[i],height[j])*(j-i));
+        if(height[i] < height[j]) i++;
+        else j--;
     }
     return maxArea;
 }
