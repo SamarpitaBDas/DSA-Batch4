7d5db514a98ec917932eee6f2fad427871cc4d5c
SamarpitaBDas <samarpitabdas@gmail.com>
2026-03-15 10:47:04 +0530

    242. Valid Anagram  -- using vector

--- 14_feb/valid_anagram.cpp
+++ 14_feb/valid_anagram.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 bool isAnagram(string s, string t){
-    //base case 
+
+    //using hashmap
+    // //base case 
+    // if(s.size()!=t.size()) return false;
+    // unordered_map<char,int> mp;
+    // for(int i=0;i<s.size();i++){
+    //     mp[s[i]]++;
+    //     mp[t[i]]--;
+    // }
+    // for(auto& pair: mp){
+    //     if(pair.second!=0) return false;
+    // }
+    // return true;
+
+
+    //using vector
+    //base case
     if(s.size()!=t.size()) return false;
-    unordered_map<char,int> mp;
+    vector<int> count(26,0);
     for(int i=0;i<s.size();i++){
-        mp[s[i]]++;
-        mp[t[i]]--;
+        count[s[i]-'a']++;
+        count[t[i]-'a']--;
     }
-    for(auto& pair: mp){
-        if(pair.second!=0) return false;
+    for(int i=0;i<26;i++){
+        if(count[i]!=0) return false;
     }
     return true;
 }
